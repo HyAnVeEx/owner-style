9c95640301ddbbd130c82a8b60678de6aa636406
Your Name <you@example.com>
2017-08-03 14:04:33 +0900

    小文字修正

--- db/migrate/20170731081300_add_column_products.rb
+++ db/migrate/20170731081300_add_column_products.rb
@@ -1,9 +1,9 @@
 class AddColumnProducts < ActiveRecord::Migration[5.0]
   def change
-    add_column :Products, :title, :text
-    add_column :Products, :area, :text
-    add_column :Products, :price, :int
-    add_column :Products, :deadline, :date
-    add_column :Products, :image, :string
+    add_column :products, :title, :text
+    add_column :products, :area, :text
+    add_column :products, :price, :int
+    add_column :products, :deadline, :date
+    add_column :products, :image, :string
   end
 end
